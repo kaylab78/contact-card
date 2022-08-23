d700bfcf50fd5f3fd1105e59f1902f5d9f245661
kaylab78 <kaylabackus78@gmail.com>
2022-08-22 19:52:05 -0500

    Added form functionality.

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,32 +1,70 @@
 // Import modules
-import  "./form";
-import "./submit";
+import "./form";
+// import "./submit";
 
 // Import CSS files
-import "../css/index.css"
+import "../css/index.css";
 
 // Import Bootstrap
-import { Tooltip, Toast, Popover } from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Import images
-import Logo from '../images/logo.png';
-import Bear from '../images/bear.png';
-import Dog from '../images/dog.png';
+import Logo from "../images/logo.png";
+import Bear from "../images/bear.png";
+import Dog from "../images/dog.png";
 
-// Add images on load
-window.addEventListener('load', function () {
-    document.getElementById('logo').src = Logo;
-    document.getElementById('bearThumbnail').src = Bear;
-    document.getElementById('dogThumbnail').src = Dog;
-  });
+// Import functions
+import { initdb, getDb, postDb } from "./database";
+import { fetchCards } from "./cards";
 
-// Import 
-import { initdb } from './database';
-
-window.addEventListener('load', function() {
+// On load event listener
+window.addEventListener("load", function () {
   initdb();
-  document.getElementById('logo').src = Logo;
-  document.getElementById('bearThumbnail').src = Bear;
-  document.getElementById('dogThumbnail').src = Dog;
+
+  // We are temporarily placing getDb() and postDb() function calls here for testing. We will move it to another event listener later.
+  getDb();
+  postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
+  fetchCards();
+
+  document.getElementById("logo").src = Logo;
+  document.getElementById("bearThumbnail").src = Bear;
+  document.getElementById("dogThumbnail").src = Dog;
+});
+
+import { toggleForm, clearForm } from './form';
+
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener("click", (event) => {
+  toggleForm();
+});
+
+form.addEventListener("submit", (event) => {
+  // Handle data
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+    fetchCards();
+    // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
 });
